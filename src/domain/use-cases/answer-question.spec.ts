b3b633420c0488cb8d56b2a95fe04530015ef9b8
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-03-24 15:36:02 -0300

    create answer-question test

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,5 +1,14 @@
-import { test } from 'vitest'
+import { expect, test } from 'vitest'
+import { AnswerQuestionUseCase } from './answer-question'
 
 test('create an answer', ()=>{
-  
+  const answerQuestion = new AnswerQuestionUseCase()
+
+  const answer = answerQuestion.execute({
+    questionId: '1',
+    instructorId: '2',
+    content: 'Nova resposta'
+  })
+
+  expect(answer.content).toEqual('Nova resposta')
 })
