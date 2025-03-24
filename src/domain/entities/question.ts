eb6c9cfd71bfc1c1aebd3f1eb90d3e9dcfc01dc9
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-03-24 15:35:39 -0300

    feat: add author id

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,13 +1,16 @@
 import { randomUUID } from "node:crypto"
 
 export class Question {
+  public id: string
   public title: string
   public content: string
-  public id: string
+  public authorId: string
+  
 
-  constructor(title: string, content: string, id?: string){
+  constructor(title: string, content: string,authorId: string, id?: string){
     this.title = title
     this.content = content
+    this.authorId = authorId
     this.id = id ?? randomUUID()
   }
 }
