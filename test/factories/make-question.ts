0b06aae070ad1b3de0d7a166b59980534a4d2dc1
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-03-27 10:44:26 -0300

    feat: add use fake data

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,17 +1,24 @@
+import { faker } from '@faker-js/faker'
+
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
 
-/* eslint-disable-next-line */
-export function MakeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityID(),
-    title: 'Test question',
-    content: 'New question',
-    ...override,
-  })
+export function MakeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
