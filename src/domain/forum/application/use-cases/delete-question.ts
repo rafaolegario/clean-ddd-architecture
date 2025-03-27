fd5efbc5224b6558ec359e12d8cbcefa8b37abd2
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-03-27 11:32:06 -0300

    fix: add and validate author id

--- src/domain/forum/application/use-cases/delete-question.ts
+++ src/domain/forum/application/use-cases/delete-question.ts
@@ -2,6 +2,7 @@ import { QuestionsRepository } from '../repositories/question-repository'
 
 interface DeleteQuestionUseCaseRequest {
   questionID: string
+  authorID: string
 }
 
 interface DeleteQuestionUseCaseResponse {}
@@ -11,6 +12,7 @@ export class DeleteQuestionUseCase {
 
   async execute({
     questionID,
+    authorID,
   }: DeleteQuestionUseCaseRequest): Promise<DeleteQuestionUseCaseResponse> {
     const question = await this.QuestionsRepository.findById(questionID)
 
@@ -18,7 +20,11 @@ export class DeleteQuestionUseCase {
       throw new Error('Question not found!')
     }
 
-    await this.QuestionsRepository.delete(questionID)
+    if (authorID !== question.authorId.toString()) {
+      throw new Error('Not allowed!')
+    }
+
+    await this.QuestionsRepository.delete(question)
 
     return {}
   }
