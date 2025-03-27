c46907945a9f32aaa5f55493b0d3b320ba20d3d6
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-03-27 11:34:30 -0300

    feat: create delete answer test

---
+++ src/domain/forum/application/use-cases/delete-answer.spec.ts
@@ -0,0 +1,29 @@
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { DeleteAnswerUseCase } from './delete-answer'
+import { MakeAnswer } from 'test/factories/make-answer'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+let inMemoryAnswerRepository: InMemoryAnswersRepository
+let sut: DeleteAnswerUseCase
+
+describe('Delete Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswerRepository = new InMemoryAnswersRepository()
+    sut = new DeleteAnswerUseCase(inMemoryAnswerRepository)
+  })
+
+  it('Should be able to delete an answer', async () => {
+    const newAnswer = MakeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1'),
+    )
+
+    await inMemoryAnswerRepository.create(newAnswer)
+
+    await sut.execute({ answerID: 'answer-1', authorID: 'author-1' })
+
+    expect(inMemoryAnswerRepository.items).toHaveLength(0)
+  })
+})
