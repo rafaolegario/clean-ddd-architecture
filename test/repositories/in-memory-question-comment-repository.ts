92bc52796993032bb881cc257f3ec45599dda0d9
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-03-28 15:49:28 -0300

    feat: Add Method Find many comments

--- test/repositories/in-memory-question-comment-repository.ts
+++ test/repositories/in-memory-question-comment-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-parms'
 import { QuestionCommentRepository } from '@/domain/forum/application/repositories/question-comment-repository'
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
 
@@ -24,5 +25,16 @@ export class InMemoryQuestionCommentRepository
     }
 
     return comment
+  }
+
+  async fetchManyCommentsByQuestionId(
+    questionID: string,
+    { page }: PaginationParams,
+  ) {
+    const comments = this.items
+      .filter((item) => item.questionId.toString() === questionID)
+      .slice((page - 1) * 20, page * 20)
+
+    return comments
   }
 }
