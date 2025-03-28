e1221751f655f31d4c8662839253e4c7e7e941e3
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-03-28 12:36:08 -0300

    test: Create on answer comment test

---
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,35 @@
+import { InMemoryAnswerCommentRepository } from 'test/repositories/in-memory-answer-comment-repository'
+import { AnswerCommentUseCase } from './comment-on-answer'
+import { MakeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+
+let inMemoryAnswerRepository: InMemoryAnswersRepository
+let inMemoryAnswerCommentRepository: InMemoryAnswerCommentRepository
+let sut: AnswerCommentUseCase
+
+describe('Create answer comment', () => {
+  beforeEach(() => {
+    inMemoryAnswerRepository = new InMemoryAnswersRepository()
+    inMemoryAnswerCommentRepository = new InMemoryAnswerCommentRepository()
+    sut = new AnswerCommentUseCase(
+      inMemoryAnswerRepository,
+      inMemoryAnswerCommentRepository,
+    )
+  })
+
+  it('Should be able to create an answer comment', async () => {
+    const answer = MakeAnswer()
+
+    await inMemoryAnswerRepository.create(answer)
+
+    await sut.execute({
+      authorID: answer.authorId.toString(),
+      answerID: answer.id.toString(),
+      content: 'content example',
+    })
+
+    expect(inMemoryAnswerCommentRepository.items[0].content).toEqual(
+      'content example',
+    )
+  })
+})
