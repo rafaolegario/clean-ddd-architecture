d3a86d7aae22338baa1104e6136b1615a3a7b638
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-04-03 13:48:01 -0300

    feat: add new methods in answers repository

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -4,6 +4,7 @@ import { Answer } from '../../enterprise/entities/answer'
 export interface AnswersRepository {
   create(answer: Answer): Promise<void>
   delete(answer: Answer): Promise<void>
+  save(question: Answer): Promise<void>
   findById(id: string): Promise<Answer | null>
   fetchManyByQuestionId(
     question: string,
